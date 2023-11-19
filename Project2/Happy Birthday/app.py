24bc1700cfc830afb27d49d3bfb228498fe4be21
Jayson056 <jaysonc864@gmail.com>
2023-11-20 04:09:13 +0800

    second  commit

---
+++ Project2/Happy Birthday/app.py
@@ -0,0 +1,48 @@
+# /Happy Birthday/app.py
+from flask import Flask, render_template, request
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+# Updated destination folder to 'static/images'
+UPLOAD_FOLDER = 'static/images'
+ALLOWED_EXTENSIONS = {'png'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def create_upload_folder():
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    student_name = request.form.get('student_name')
+    birthday_info = request.form.get('birthday_info')
+    birth_Date = request.form.get('birth_Date')
+    current_date = datetime.now().strftime("%B-%d")
+
+    # Create the upload folder if it doesn't exist
+    create_upload_folder()
+
+    # Handle image upload
+    if 'image_upload' in request.files:
+        image_file = request.files['image_upload']
+        if image_file and allowed_file(image_file.filename):
+            filename = 'background.png'  # Save the image with a fixed filename
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image_file.save(image_path)
+
+    # Process data and generate pubmat picture (implement this logic)
+
+    return render_template('result.html', student_name=student_name, birthday_info=birthday_info, background_image=image_path, current_date=current_date)
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ Project2/Happy Birthday/config.py
@@ -0,0 +1,16 @@
+# /bsit_website/config.py
+# Add your configuration settings here
+
+# /bsit_website/config.py
+
+# Flask configuration
+DEBUG = True  # Set to False in production
+SECRET_KEY = 'your_secret_key'  # Replace with a secure, random key
+
+# Image processing configuration
+IMAGE_FONT_PATH = 'path/to/your/font.ttf'  # Replace with the path to your font file
+IMAGE_FONT_SIZE = 30  # Adjust font size as needed
+IMAGE_WIDTH = 800  # Adjust image width as needed
+IMAGE_HEIGHT = 600  # Adjust image height as needed
+IMAGE_BACKGROUND_COLOR = 'white'  # Adjust background color as needed
+
